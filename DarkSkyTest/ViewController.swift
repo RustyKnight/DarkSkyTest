044089e02f79837982cd7a3b4d12554478a17652
Shane Whitehead <shane.whitehead@beamcommunications.com>
2018-10-17 16:38:45 +1100

    initial commit

---
+++ DarkSkyTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DarkSkyTest
+//
+//  Created by Shane Whitehead on 17/10/18.
+//  Copyright © 2018 Beam Communications. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+
+}
+
